a88bb20443af02694736e23934d44b39e7368877
Subhajit Sarkar <sarkarsubhajit9499@gmail.com>
2023-01-25 10:27:03 +0530

    resetting quiz and score added

--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({Key key}) : super(key: key);
+  final int resultScore;
+  final VoidCallback resetQuiz;
+  Result(this.resultScore, this.resetQuiz);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You\'re awesome and innocent!';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable!';
+    } else if (resultScore <= 16) {
+      resultText = 'You\'re strange!';
+    } else {
+      resultText = 'You\'re bad!';
+    }
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it!'),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          TextButton(onPressed: resetQuiz, child: Text('Restart quiz!')),
+        ],
+      ),
     );
   }
 }
